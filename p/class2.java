0de90dd277f5f4e740d59caf40b5747bea84317f
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-01-19 10:34:37 +0900

    Add files via upload

---
+++ p/class2.java
@@ -0,0 +1,129 @@
+package day2;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int a= 10;
+//System.out.println(a);
+//System.out.println(a+10);
+//System.out.println(a+a+2a);
+//
+//int a=4;
+//int b=5;
+//
+//System.out.println(a+" "+b);
+
+//int a=5;
+//int b=7;
+//
+//System.out.println("가을이와 겨울이를 소개하겠습니다!");
+//System.out.println("가을이의 나이는 "+b+"살이고 겨울이의 나이 "+a+"살입니다.");
+		
+//		int a=10;
+//		int b=20;
+//		System.out.println("a : " +a);
+//		System.out.println("b : "+b);
+		
+//		int a=3;
+//		int b=5;
+//		
+//		System.out.println("가을이의 인형은 "+a+"이 겨울이의 인형은 "+b+"이다.");
+//		System.out.println("가을이와 겨울이는 인형을 "+(a+b)+"개 가지고 있다.");
+		
+//		int a=100;
+//		int b=150;
+//		
+//		System.out.println("a의 값은 "+a);
+//		System.out.println("b의 값은 "+b);
+//		System.out.println("a와 b의 합은 "+(a+b));
+//		System.out.println("a와 b의 차는 "+(a-b));
+		
+//		double a=185.7;
+//		double b=60.4;
+//		
+//		System.out.println("[A의 신체 정보]");
+//		System.out.println("키 : "+a);
+//		System.out.println("몸무게 : "+b);
+		
+//		double x=3.18;
+//		double y=2.5;
+//		
+//		System.out.println("x : "+x);
+//		System.out.println("y : "+y);
+//		System.out.println("x와 y의 합은 "+(x+y));
+		
+		
+//	char a='A';
+//	String b="AB";
+//	String c="apple";
+//	String d="Hello Java";
+//	
+//	System.out.println(a);
+//	System.out.println(b);
+//	System.out.println(c);
+//	System.out.println(d);
+//	
+		
+//		char a= 'A';
+//		char b= 'B';
+//		char c= 'C';
+//		
+//		System.out.println("[Grade List]");
+//		System.out.println("Tom : "+a);	
+//		System.out.println("Amy : "+b+"+");
+//		System.out.println("Jane : "+c+"-");	
+//	
+////	
+//		String a ="Apple";
+//		String b ="Bear";
+//		String c= "Cat";
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+		
+//		String a= "가을";
+//		int b =20;
+//		char c ='A';
+//		
+//		System.out.println("이름 :"+a);
+//		System.out.println("나이 :"+b);
+//		System.out.println("성적 :"+c);
+
+//		
+//		float a=4.05f;
+//		short b=100;
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+		
+//		int x=5;
+//		int y =10;
+//		int a;
+//		System.out.println("[교환 전]");
+//		System.out.println("x : "+x);
+//		System.out.println("y : "+y);
+//		
+//		a=x;
+//		x=y;
+//		y=a;
+//	
+//		System.out.println("[교환 후]");
+//		System.out.println("x : "+x);
+//		System.out.println("y : "+y);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
