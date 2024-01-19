ac26181737cc861134e41b65156b70ed60605311
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-01-19 10:33:41 +0900

    Add files via upload

---
+++ p/_1.java
@@ -0,0 +1,46 @@
+package day1;
+
+public class _1 {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello");
+		
+		//System.out.println("안녕");
+//		System.out.println(10);
+//		System.out.println(20);
+//		System.out.println(0.0);
+//		System.out.println(1);
+//		System.out.println(-0.2);
+//		System.out.println(1+1);
+//		System.out.println(5-1);
+//		System.out.println("안녕하세요\n");
+//		System.out.println("오늘은 자바 첫 수업입니다.\n");
+//		System.out.println("자바는 생각보다 재밌어요!");
+//		System.out.println("0.01");
+//		System.out.println("-0.2");
+//		System.out.println(10);
+//		System.out.println(20);
+//		System.out.println('3');
+//		
+//		System.out.println("안녕하세요.제 소개를 하겠습니다.");
+//		System.out.println("저의 이름은 \'민지\'입니다.");
+//		System.out.println("저는 \"20살\"입니다.");
+//		System.out.println("잘 부탁드립니다!");
+//		System.out.println("Hello.\nMy name is Tom!/)_)('");
+//		
+//		System.out.println("\\  /\\");
+//		System.out.println(")  ( \')");
+//		System.out.println("(  /  )");
+//		System.out.println(" \\(__)|");
+//		
+//		System.out.println("|\\_/|");
+//		System.out.println("|q p|   /}");
+//		System.out.println("( 0 )\"\"\"\\");
+//		System.out.println("|\"^\"`    |");
+//		System.out.println("||_/=\\\\__|");
+//
+		
+		
+	}
+
+}
