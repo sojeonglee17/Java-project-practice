1943bfae7f9d4957884ccb2fce1443c594705e5d
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-01-20 10:37:28 +0900

    Add files via upload

---
+++ p/day4.java
@@ -0,0 +1,42 @@
+package day4;
+
+public class day4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+//		int a=7, b=5;
+//		
+//		System.out.println("직사각형 넓이 : "+(a*b));
+		
+//		
+//		int a=6,b=3;
+//		
+//		
+//		System.out.println("두 수의 합 : "+(a+b));
+//		System.out.println("두 수의 차 : "+(a-b));
+//		System.out.println("두 수의 곱 : "+(a*b));
+//		System.out.println("두 수의 몫 : "+(a/b));
+
+//	
+//	int a=5,b=7;
+//	int c=a*b;
+//	
+//	
+//	System.out.println("직사각형의 넓이: "+c);
+//	
+//	
+//		
+//		double y= 12.2*6.3;
+//		double x= 10.5*8.4;
+//		
+//		double force = x/y;
+//		
+//		System.out.println("x 포탄의 힘은 "+x+"y 포탄의 힘은 "+y);
+//		System.out.println("x 포탄이 y 포탄보다 "+force+"배 더 세다.");
+
+	}
+
+}
