039c0f7a28316eb8a829751bf3b431896ae2c639
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-01-20 10:46:24 +0900

    Add files via upload

---
+++ homework/homework_1_19.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package homework;
+
+
+public class homework_1_19 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+/*1._1234번 문*/		
+		
+		/*System.out.println("첫 번째 프로그램입니다.")*/
+		
+		/*System.out.println("Fun Programming!");
+		
+		String a="Fun Programming!";
+		System.out.println(a);*/
+		
+	/*	String c="두 번째 프로그램입니다.";
+		String d="한 줄에 여러 문장을 출력합니다.";
+		
+	System.out.println(c+d); */
+		
+	/*	String e= "Programming!";
+		String f="It's fun.";
+		
+		System.out.println(e+" "+f);*/
+		
+		
+		
+/*2.*/
+/*	System.out.println("세 번째 프로그램입니다.");
+	System.out.println("줄을 바꾸어 출력합니다.");
+	System.out.println("여기까지 출력하고");
+	System.out.println("줄을 바꿉니다.");*/
+	
+/*	System.out.println("My name is Hong Gil Dong");
+	System.out.println("I am 13 years old");  */
+		
+	/*	System.out.println("    "+" @@ ");
+		System.out.println("  @"+"  "+"@  ");
+		System.out.println(" @"+"    "+"@ ");
+		System.out.println("@"+"      "+"@");
+		System.out.println(" @"+"   "+" @");
+		System.out.println("  @"+"  "+"@  ");
+		System.out.println("   "+"@@"+"   ");
+	*/
+		
+		
+		
+	/*	System.out.println("(@)"+" "+"(@)");
+		System.out.println("(=^.^=)");
+		System.out.println("(-m-m-)");
+	*/
+	
+		
+	/*
+	System.out.println("수를 출력할때는 따옴표를 생략합니다.");
+	System.out.println(123);
+	System.out.println("6.5"+"000");
+	
+	*/
+	
+		
+		
+/*		
+	System.out.println("My height");
+	System.out.println(170);
+	System.out.println("My weight");
+	System.out.println("68.6");
+		
+*/
+		
+/*
+		int a=10;
+		int b=5;
+		int c=2005,d=6,e=17;
+		
+		
+		System.out.println("수식을 출력하 계산 결과가 출력됩니다.");
+		System.out.println("a+b="+(a+b));
+		System.out.println("내 생일은 "+c+"년 "+d+"월 "+e+"일 입니다.");
+		
+*/
+
+		
+		
+/*
+	 int a=5;
+	
+	 System.out.println(a+" Dan");
+	 System.out.println(a+" * 2 ="+(a*2));
+	 
+*/
+		
+
+/*
+		System.out.println("subject score");
+		System.out.println("==============");
+		System.out.println("  korean    90");
+		System.out.println(" english   100");
+		System.out.println("computer    80");
+*/
+		
+	}
+
+}
