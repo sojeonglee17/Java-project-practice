4d1949968dec01f0163c2bc697db22948cd04cc3
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-01-23 19:50:41 +0900

    Add files via upload

---
+++ homework/homework_1_20.java
@@ -0,0 +1,138 @@
+package homework;
+
+public class homework_1_20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//출력문제 1
+		
+/*		System.out.println("  item       count        price");
+		System.out.println("   pen          20          100");
+		System.out.println("  note           5           95");
+		System.out.println("eraser         110           97");
+*/
+		
+		//출력문제 2
+/*		
+ 		System.out.println("My name is Hong");
+*/		
+		
+		//출력문제 3
+/*		
+		System.out.println("My hometown");
+		System.out.println("Flowering mountain");
+*/
+		
+		//출력문제 4
+/*		
+		System.out.println("TTTTTTTTTT");
+		System.out.println("TTTTTTTTTT");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");
+*/		
+		
+		//출력문제 5
+/*		
+		System.out.println("  Seoul     10,312,545        +91,375");
+		System.out.println("  Busan      3,567,910         +5,868");
+		System.out.println("Incheon      2,758,296        +64,888");
+		System.out.println("  Daegu      2,511,676        +17,230");
+		System.out.println("Gwangju      1,454,636        +29,774");
+*/		
+		
+		//출력문제 6
+		
+/*		int a=10,b=A;
+		
+	    System.out.println("a = "+a);
+	    System.out.println("b = "+b);
+*/		
+	
+		//출력문제 7
+
+/*
+	    int i=-100;
+	    
+	    System.out.println(i);
+*/	    
+		
+		//출력문제 8
+		
+/*		int x=10,y=20;
+		int z;
+	
+		z=x;
+		x=y;
+		y=z;
+
+		System.out.println(x);
+		System.out.println(y);//이거 x와 y 한줄에 표현하고 싶은데 표현이 안되어요...
+
+*/		
+		//출력문제 9
+		
+/*
+ 	short a=-1;
+	int b=100;
+	
+	System.out.println(a);
+	System.out.println(b);
+*/	
+		
+		//출력문제 10
+/*
+		int a=10,b=20;
+		int x,y;
+		
+		System.out.println(a+"+"+b+"="+(a+b));
+		
+		x=a;
+		y=b;
+		
+		a=30;
+		b=40;
+		
+		System.out.println(a+"+"+b+"="+(a+b));
+*/		
+		
+		//출력문제 11
+/*		
+		short a=55,b=10;
+		int x=2008,y=1999;
+		
+		System.out.println(a+"-"+b+"="+(a-b));
+		System.out.println(x+"-"+y+"="+(x-y));
+*/
+		
+		
+		//출력문제 12
+		
+/*		
+			//1단순 출력
+		System.out.println("넓이 : 5 * 5 * 3.14 =	 78.5");
+		
+			//2.변수 사용
+		
+		int a=5;
+		double x=3.14;
+		
+		System.out.println("넓이 : "+a+"*"+a+"*"+x+"="+(a*a*x));
+
+*/
+		
+		
+		//출력문제 13
+		
+/*
+		
+		int weight = 49;
+		double gravity = 0.2689;
+		
+		System.out.println(weight+"*"+gravity+"="+(weight*gravity));
+
+*/	
+	
+	}
+
+}
