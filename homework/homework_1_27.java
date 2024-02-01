2ca41fec37a6ff7a831e7e6b343f3f0f0f428e5b
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-02-01 17:52:14 +0900

    Add files via upload

---
+++ homework/homework_1_27.java
@@ -0,0 +1,121 @@
+package homework;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+                 
+public class homework_1_27 {
+
+	public static void main (String[] args) throws IOException{
+		BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(num.readLine());
+	
+		int a=Integer.parseInt(st.nextToken());
+		int b=Integer.parseInt(st.nextToken());
+		int c=Integer.parseInt(st.nextToken());
+		
+		System.out.println(a+b+c);
+		
+		
+		
+		//			String A= num.readLine();
+	
+   //문제를 읽었을 때 첫 줄에 age가 주어진다<<이게 어떤 의미인지 모르겠습니다.
+//	처음 문제가 나왔을때 뭐 먼저 표시하라는것인지 살짝 헷갈립니다.
+	
+//	System.out.print("당신의 나이는 몇 살 입니까? : ");
+//	int age = Integer.parseInt(num.readLine());
+//	System.out.println("당신의 나이는 "+age+"살이군요.");
+//
+//	
+//	System.out.print("height = " );
+//	int height = Integer.parseInt(num.readLine());
+//	System.out.println("Your height is "+height+"cm");
+	
+//
+//int a=Integer.parseInt(num.readLine());	
+//	System.out.println(a);
+	
+//double a = Double.parseDouble(num.readLine());
+//System.out.println(a);
+
+//	System.out.println(A);		
+
+	
+/*	System.out.println("[Tom List]");
+	System.out.print("name : ");
+	String name=num.readLine();
+	
+
+	System.out.println("age : ");
+	int age=Integer.parseInt(num.readLine());
+	System.out.println("height : ");	
+	double height = Double.parseDouble(num.readLine());
+	System.out.println("weight : ");
+	double weight = Double.parseDouble(num.readLine());
+	
+	System.out.println("");
+	System.out.println("[당신이 입력한 tom의 정보]");
+	System.out.println("name : "+name);
+	System.out.println("age : "+age);
+	System.out.println("height : "+height);
+	System.out.println("weight : "+weight);
+*/
+		
+		
+	
+//	String a=num.readLine();
+//	System.out.print(a);
+	
+		
+//		char a = num.readLine().charAt(0);
+//	System.out.println(a);
+		
+		
+/*	System.out.print("grede1 : ");
+	char grade1= num.readLine().charAt(0);
+	System.out.print("grade2 : ");
+	char grade2= num.readLine().charAt(0);
+	System.out.print("score3 : ");
+	char score3=num.readLine().charAt(0);
+	
+	System.out.println("");
+	System.out.println("[최종 결과]");
+	System.out.println("grede1 : "+grade1+"+");
+	System.out.println("grade2 : "+grade2+"-");
+	System.out.println("score3 : "+score3);
+*/
+/*
+		
+		//세로 입력
+int a = Integer.parseInt(num.readLine());
+int b = Integer.parseInt(num.readLine());
+
+double c = Double.parseDouble(num.readLine());
+double d = Double.parseDouble(num.readLine());
+
+String e = num.readLine();
+String f = num.readLine();
+
+char g = num.readLine().charAt(0);
+char h = num.readLine().charAt(0);
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+*/
+
+//가로 입
+		
+//		import java.util.StringTokenizer<<이거 적어줘야함
+
+	}
+}
