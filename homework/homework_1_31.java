67a2c371b3ab8b5ab9475f5c639fa1b9d6d3a1b6
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-02-13 22:18:30 +0900

    Add files via upload

---
+++ homework/homework_1_31.java
@@ -0,0 +1,69 @@
+package homework;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class homework_1_31 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());//*br이 들어가야한다.
+		
+ 
+//	
+//	
+//	int A = Integer.parseInt(st.nextToken());//가로 작성 가
+//	int B = Integer.parseInt(st.nextToken());
+//	int C = Integer.parseInt(st.nextToken());
+//	
+//	
+//	
+//	System.out.println(A+B+C);//StringTokenizer st = new StringTokenizer(br.readLine());가 활성화 되어 있으면 프로그램 오류 뜨던데 있으면 안되는건가요?
+//		
+//	int A = Integer.parseInt(st.nextToken());
+//	int B = Integer.parseInt(st.nextToken());
+//	
+//	System.out.println(A+B);
+//	System.out.println(A-B);
+//	System.out.println(A*B);
+//	System.out.println(A/B);
+//	System.out.println(A%B);
+//	
+//		
+//		불기 연도를 서기 연도로 변환
+//	int y = Integer.parseInt(br.readLine());
+//	
+//	System.out.println(y-543);
+
+		
+		//(A+B)%C는 ((A%C)+(B%C))%C와 같을까?
+		//(A*B)%C는 ((A%C)*(B%C))%C와 같을까?
+		
+		
+// int A = Integer.parseInt(st.nextToken());
+// int B = Integer.parseInt(st.nextToken());
+// int C = Integer.parseInt(st.nextToken());
+//	
+// 
+//System.out.println((A+B)%C);
+//System.out.println(((A%C)+(B%C))%C);
+//System.out.println((A*B)%C);
+//System.out.println(((A%C)*(B%C))%C);
+
+		
+//		(세 자리 수) * (세 자리 수)
+
+//		int num1 = Integer.parseInt(br.readLine());
+//		int num2 = Integer.parseInt(br.readLine());
+//		
+//	
+//
+//	System.out.println(num1*(num2%10));
+//	System.out.println(num1*((num2/10)%10));
+//	System.out.println(num1*((num2/10))/10);
+//	System.out.print(num1*num2);
+	
+	
+	}
+}
