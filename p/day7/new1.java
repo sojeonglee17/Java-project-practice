a81ebb09a3e7c9ad52b4b228d46e824907a0739e
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-02-13 22:23:46 +0900

    Add files via upload

---
+++ p/day7/new1.java
@@ -0,0 +1,94 @@
+package day7;
+
+import java.util.Scanner;
+
+public class new1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		
+//		int a = sc.nextInt();
+//		System.out.println(a);
+		
+//		double a= sc.nextDouble();
+//		System.out.println(a);
+//		
+//		float b = sc.nextFloat();
+//		System.out.println(b);
+		
+
+		
+		
+//		
+//		System.out.print("사과의 개수 : ");
+//		int a = sc.nextInt();
+//		System.out.print("귤의 개수 : ");
+//		int b = sc.nextInt();
+//		System.out.print("딸기의 개수 : ");
+//		int c = sc.nextInt();
+//		
+//		System.out.println("과일의 종류는 3가지");
+//		System.out.print("전체의 값은 "+(a+b+c)+"개 입니다");
+		
+//		System.out.print("1번 학생의 키 : ");
+//		double a = sc.nextDouble();
+//		System.out.print("2번 학생의 키 : ");
+//		double b =sc.nextDouble();
+//		
+//		System.out.println("1번과 2번 학생의 키의 합은 "+(a+b)+"이고");
+//		System.out.print("평균은 "+((a+b)/2)+"이다.");
+
+//	* 문자열형 String a= sc.nextLine();
+		// * 문자형 char a = sc.next().charAt(0);
+		
+		
+//		String a = sc.nextLine();
+//		System.out.println(a);
+//		
+//		char b = sc.next().charAt(0);
+//		System.out.println(b);
+
+		//		1
+//		int a = sc.nextInt();
+//		System.out.print(a);
+		
+			//2		
+		
+//		double a = sc.nextDouble();
+//		System.out.print(a);
+		
+		
+//		String a =sc.nextLine();
+//		System.out.print(a);
+//		
+//		char b = sc.next().charAt(0);
+//		System.out.print(b);
+		
+//int a = sc.nextInt();
+//int b = sc.nextInt();
+//
+//System.out.print(a+" "+b);
+
+		
+//		int a= sc.nextInt();
+//		int b= sc.nextInt();
+//		
+//		System.out.println("입력 받은 두 변수의 합 : "+(a+b));
+//		System.out.println("입력 받은 두 변수의 차 : "+(a-b));
+		
+//		System.out.print("첫 번째 수 입력 : ");
+//		int a = sc.nextInt();
+//		System.out.print("두 번째 수 입력 : ");
+//		double b = sc.nextDouble();
+//		
+//		
+//		System.out.println("");
+//		System.out.println(a);
+//		System.out.println(b);
+		
+		
+		sc.close();//적어주지 않으면 메모리를 계속해서 사용하기 때문에 적어줘야한다.누수를 방지 
+		
+	}
+
+}
