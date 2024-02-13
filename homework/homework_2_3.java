a85d3709610b5a2ad44c027740d76b621fecc54e
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-02-13 22:20:19 +0900

    Add files via upload

---
+++ homework/homework_2_3.java
@@ -0,0 +1,70 @@
+package homework;
+//import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+//public class homework_2_3 {
+//
+//	public static void main(String[] args) throws IOException{
+//			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//			StringTokenizer st= new StringTokenizer(br.readLine());
+//	}
+//			
+////		Scanner sc = new Scanner(System.in);
+//		
+//	//종합문제
+//	//1
+//		
+////	int A = sc.nextInt();
+////	int B = sc.nextInt();
+////	
+////	System.out.println(A+B);
+////	System.out.println(A-B);
+////	System.out.println(A*B);
+////	System.out.println(A/B);
+////	System.out.println(A%B);
+//	
+//	//2
+//	
+////	int year = sc.nextInt();
+////	System.out.println(year-543);
+//	
+//	//3
+//	
+////	int A = sc.nextInt();
+////	int B = sc.nextInt();
+////	int C = sc.nextInt();
+////	
+////	System.out.println((A+B)%C);
+////	System.out.println(((A%C)+(B%C))%C);
+////	System.out.println((A*B)%C);	
+////	System.out.println(((A%C)*(B%C))%C);
+//	
+//	//4
+//	
+////	int a = sc.nextInt();
+////	int b = sc.nextInt();
+////	
+////	
+////	System.out.println(a*(b%10));
+////	System.out.println(a*((b/10)%10));
+////	System.out.println(a*((b/10)/10));
+////	System.out.println(a*b);
+////	
+//	
+////	int a = sc.nextInt();
+////	int b = sc.nextInt();
+////	System.out.println(a+" + "+b+" = "+(a+b));
+////	System.out.println(a+" * "+b+" = "+(a*b));
+//	
+//		int c = Integer.parseInt(st.readLine);
+//		int d = Integer.parseInt(st.readLine);
+//	
+
+		
+		
+//		sc.close();
+
