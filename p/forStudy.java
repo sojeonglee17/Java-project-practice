9be8a9e44817db03c3668d7ab5ffd2cff70974d8
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-03-12 15:09:36 +0900

    Add files via upload

---
+++ p/forStudy.java
@@ -0,0 +1,69 @@
+package day13;
+
+import java.util.Scanner;
+
+public class forStudy {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+//		for(int i =15; i <25; i++) {
+//			System.out.println("apple");
+//		}
+
+		
+		
+//		for(int i =5; i<=5&&i>2; i--) {
+//			System.out.print("ABC");
+//		}//탈출
+//		System.out.print("D");
+		
+//		int sum = 0;
+//	for(int i=1;i<11;i++) {
+//		sum=sum + i;
+//	
+//	}
+//	System.out.println("1부터 10까지의 합 "+sum);
+
+		
+//		int sum = 0;
+//		for(int i=2;i<11;i++) {
+//			if(i%2==0) {
+//				sum+=i;
+//			}
+//		}
+//		System.out.println("2부터 10까지의 숫자 중 짝수의 합은 "+sum);
+
+//		for(int i =1;i<=100;i++) {
+//			if(i%4==0) {
+//				System.out.println("A");
+//			}else if(i%4==1) {
+//				System.out.println("B");
+//			}else if(i%4==2) {
+//				System.out.println("C");
+//			}else {
+//				System.out.println("D");
+//			}
+//		}
+//		
+//		
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		
+//		int sum = 0;
+//		
+//		
+//		for(int i=a;i<=b;i++) {
+//			sum+=i;
+//			}
+//				System.out.println(sum);
+			
+		
+		
+		sc.close();
+	}
+
+}
