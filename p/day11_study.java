2b9d94d9407126364285bb2dd5b422a83c9c60d7
sojeonglee17 <155560081+sojeonglee17@users.noreply.github.com>
2024-03-12 15:08:18 +0900

    Add files via upload

---
+++ p/day11_study.java
@@ -0,0 +1,87 @@
+package day11;
+
+
+import java.util.Scanner;
+
+public class day11_study {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+
+		
+//		int year = sc.nextInt();
+//		
+//		if((year%4==0)&&(year%100!=0)||(year%400==0)) {
+//			System.out.println("1");
+//		}else {
+//			System.out.println("0");
+//		}
+//		
+		
+//	int a = sc.nextInt();
+//	int b = sc.nextInt();
+//	
+//	if((a>0)&&(b>0)){
+//		System.out.println("1");
+//	}else if((a<0)&&(b>0)){
+//		System.out.println("2");
+//	}else if((a<0)&&(b<0)) {
+//		System.out.println("3");
+//	}else if((a>0)&&(b<0)) {
+//		System.out.println("4");
+//	}
+//		
+		
+		
+//	int a = sc.nextInt();
+//	int b = sc.nextInt();
+//	int c = sc.nextInt();
+//	
+//	
+//	if((a==b)&&(a==c)&&(b==c)){
+//		System.out.println(10000+(a*1000));
+//	}else if(a==b){
+//		System.out.println(1000+a*100);
+//	}else if(a==c) {
+//		System.out.println(1000+a*100);
+//	}else if(b==c) {
+//		System.out.println(1000+b*100);
+//	}else {
+//		System.out.println(Math.max(a, Math.max(b,c))*100);
+//	}		
+			
+			//math.max(x,y); : 두 값 중 더 값 반환 메서드
+			
+//	int i = 1;
+//	
+//	while(i<11){
+//		System.out.println(i);
+//		i++;
+
+//	int i = 0;
+//	
+//	while(i<10) {
+//		i++;
+//		System.out.println(i);
+//	
+
+		
+	int i = 1;
+
+		while(i<=10) {
+			if(i%2==0) {
+			System.out.println(i);
+	}
+	
+	i++;
+		}
+		
+		
+		
+		sc.close();
+		
+	}
+
+	}
+	
+
